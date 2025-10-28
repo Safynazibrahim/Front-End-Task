f72eac3cd13bd8b718147af4ecdd24ce7f4a2d03
Safynazibrahim <safynazibrahim4@gmail.com>
2025-10-28 06:13:13 +0300

    part of create post page is done

--- src/app/components/content-management/create-post/create-post.component.ts
+++ src/app/components/content-management/create-post/create-post.component.ts
@@ -1,11 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-create-post',
-  imports: [],
+  imports: [CarouselModule,ButtonModule,TagModule],
   templateUrl: './create-post.component.html',
-  styleUrl: './create-post.component.css'
+  styleUrls: ['./create-post.component.css']
 })
-export class CreatePostComponent {
+export class CreatePostComponent implements OnInit {
+
+
+
+  products =[
+    {
+    image: 'bamboo-watch.jpg',
+    date:'1/12/2025'
+},
+    {
+    image: 'bamboo-watch.jpg',
+    date:'1/12/2025'
+},
+    {
+    image: 'bamboo-watch.jpg',
+    date:'1/12/2025'
+},
+    {
+    image: 'bamboo-watch.jpg',
+    date:'1/12/2025'
+},
+    {
+    image: 'bamboo-watch.jpg',
+    date:'1/12/2025'
+},
+    {
+    image: 'bamboo-watch.jpg',
+    date:'1/12/2025'
+},
+{
+    image: 'bamboo-watch.jpg',
+    date:'1/12/2025'
+},
+    {
+    image: 'bamboo-watch.jpg',
+    date:'1/12/2025'
+},
+    {
+    image: 'bamboo-watch.jpg',
+    date:'1/12/2025'
+},
+    {
+    image: 'bamboo-watch.jpg',
+    date:'1/12/2025'
+},
+    {
+    image: 'bamboo-watch.jpg',
+    date:'1/12/2025'
+},
+    {
+    image: 'bamboo-watch.jpg',
+    date:'1/12/2025'
+},
+  ]
+
+    responsiveOptions: any[] | undefined;
+
+    constructor() {}
+
+    ngOnInit() {
+
+       this.responsiveOptions = [
+            {
+                breakpoint: '1400px',
+                numVisible: 4,
+                numScroll: 1
+            },
+            {
+                breakpoint: '1199px',
+                numVisible: 3,
+                numScroll: 1
+            },
+            {
+                breakpoint: '767px',
+                numVisible: 2,
+                numScroll: 1
+            },
+            {
+                breakpoint: '575px',
+                numVisible: 1,
+                numScroll: 1
+            }
+        ]
+    }
+
 
 }
